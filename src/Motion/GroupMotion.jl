73d7c488e4bcc4b458d1d4bd6220e4c6843faefa
Olivier Verdier <olivier.verdier@gmail.com>
2024-09-15 10:49:59 +0200

    MNT: remove deprecated function

--- src/Motion/GroupMotion.jl
+++ src/Motion/GroupMotion.jl
@@ -11,7 +11,7 @@ swap_group_motion(m::TranslationMotion{LeftSide}) = RigidMotion(GroupOperationAc
 
 # _swap_inv(::GroupOperationAction, G, χ) = inv(G,χ)
 # _swap_inv(::GroupOperationAction{LeftAction, RightSide}, G, χ) = χ
-_swap_adjoint_action(::GroupOperationAction{LeftAction, LeftSide}, G, χ, ξ) = GU.inverse_adjoint_action(G, χ, ξ)
+_swap_adjoint_action(::GroupOperationAction{LeftAction, LeftSide}, G, χ, ξ) = adjoint_action(G, χ, ξ, RightAction())
 _swap_adjoint_action(::GroupOperationAction{LeftAction, RightSide}, G, χ, ξ) = adjoint_action(G, χ, ξ)
 _swap_group_action(A::GroupOperationAction{LeftAction, LeftSide}) = GroupOperationAction(base_group(A), (LeftAction(), RightSide()))
 _swap_group_action(A::GroupOperationAction{LeftAction, RightSide}) = GroupOperationAction(base_group(A))
